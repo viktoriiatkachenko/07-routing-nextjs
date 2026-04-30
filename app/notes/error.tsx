e396602fa69aee0989d9dbef2722225df92394f7
Tamas Faradi <faradi.tamas@xfbanalytics.hu>
2026-04-30 12:20:10 +0200

    delete extra

--- app/notes/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-interface NotesErrorProps {
-  error: Error & { digest?: string };
-}
-
-export default function NotesError({ error }: NotesErrorProps) {
-  return <p>Could not fetch the list of notes. {error.message}</p>;
-}
